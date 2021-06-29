a20b01be9367648b50e232cee7eb932b26fb8057
Your Name <86529216+BrunoWalys@users.noreply.github.com>
2021-06-29 16:21:30 -0300

    teste ini

---
+++ myfirtproject/myfirtproject/src/test/java/com/devsuperios/myfirtproject/MyfirtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperios.myfirtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
